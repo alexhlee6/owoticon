2be64c8d9ca1227cd3f9564c9e3a067fb88dc16a
alex629lee <alex629lee@yahoo.com>
2020-02-21 19:05:43 -0800

    Add app loading

--- App.js
+++ App.js
@@ -3,6 +3,8 @@ import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { createStackNavigator } from '@react-navigation/stack';
+import { AppLoading } from "expo";
+import { Asset } from 'expo-asset';
 
 import DrawerContent from "./components/DrawerContent";
 import Header from "./components/Header";
@@ -12,18 +14,42 @@ import MoodScreen from "./components/screens/MoodScreen";
 const Drawer = createDrawerNavigator();
 const Stack = createStackNavigator();
 
-export default function App(props) {
-  return (
-    <NavigationContainer>
-      <Header />
+export default class App extends React.Component {
+  state = {
+    isReady: false
+  }
 
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} options={styles.options} />
-        <Stack.Screen name="Mood" component={MoodScreen} options={styles.options} />
-      </Stack.Navigator>
+  async _cacheResourcesAsync() {
+    const images = [require('./assets/icon.png')];
 
-    </NavigationContainer>
-  );
+    const cacheImages = images.map(image => {
+      return Asset.fromModule(image).downloadAsync();
+    });
+    return Promise.all(cacheImages);
+  }
+
+  render() {
+    if (!this.state.isReady) {
+      return (
+        <AppLoading
+          startAsync={this._cacheResourcesAsync}
+          onFinish={() => this.setState({ isReady: true })}
+          onError={console.warn}
+        />
+      );
+    }
+    return (
+      <NavigationContainer>
+        <Header />
+
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={HomeScreen} options={styles.options} />
+          <Stack.Screen name="Mood" component={MoodScreen} options={styles.options} />
+        </Stack.Navigator>
+
+      </NavigationContainer>
+    );
+  }
 }
 
 const styles = {
