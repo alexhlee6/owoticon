fcf9b30596292f253718910059ff6b3d367b5132
alex629lee <alex629lee@yahoo.com>
2020-02-27 12:34:27 -0800

    Add share menu and haptic touch

--- components/screens/MoodScreen.js
+++ components/screens/MoodScreen.js
@@ -1,6 +1,7 @@
 import React from "react";
-import { ScrollView, Text, View, Clipboard, TouchableOpacity } from 'react-native';
+import { ScrollView, Text, View, Clipboard, TouchableOpacity, Share } from 'react-native';
 import { EMOTES, styles } from "./util/MoodUtil";
+import * as Haptics from 'expo-haptics';
 
 class MoodScreen extends React.Component {
   constructor(props) {
@@ -30,11 +31,42 @@ class MoodScreen extends React.Component {
     }
   }
 
+  handleLongPress(str) {
+    return async () => {
+      try {
+        Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Heavy);
+        await Share.share({
+          message: str
+        }, {
+          excludedActivityTypes: [
+            "com.apple.UIKit.activity.PostToWeibo",
+            "com.apple.UIKit.activity.Mail",
+            "com.apple.UIKit.activity.Print",
+            "com.apple.UIKit.activity.AssignToContact",
+            "com.apple.UIKit.activity.SaveToCameraRoll",
+            "com.apple.UIKit.activity.AddToReadingList",
+            "com.apple.UIKit.activity.PostToFlickr",
+            "com.apple.UIKit.activity.PostToVimeo",
+            "com.apple.UIKit.activity.PostToTencentWeibo",
+            "com.apple.UIKit.activity.AirDrop",
+            "com.apple.UIKit.activity.OpenInIBooks",
+            "com.apple.UIKit.activity.MarkupAsPDF",
+            "com.apple.reminders.RemindersEditorExtension",
+            "com.apple.mobilenotes.SharingExtension",
+            "com.apple.mobileslideshow.StreamShareService",
+          ]
+        });
+      } catch (error) {
+        console.log(error.message);
+      }
+    }
+  }
+
   render() {
     if (!this.state.emotes) return null;
 
     const emotes = this.state.emotes.map((str, i) => (
-      <TouchableOpacity key={i} style={styles.emoteBackground} onPress={this.handleTouch(str)}>
+      <TouchableOpacity key={i} style={styles.emoteBackground} onPress={this.handleTouch(str)} onLongPress={this.handleLongPress(str)}>
         {this.state.justTouched === str ? <Text style={styles.copiedText}>Copied!</Text> : null }
         <Text key={i} style={styles.emoteText}>
           {str}
